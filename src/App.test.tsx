0d4d16c841594c24f72cc85a1f297d50334ae59d
Souhail Gana <souhailgana943@gmail.com>
2026-01-12 20:08:37 +0100

    deleted test components

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-describe('App Component', () => {
-    test('renders without crashing', () => {
-        render(<App />);
-        expect(document.body).toBeInTheDocument();
-    });
-});
